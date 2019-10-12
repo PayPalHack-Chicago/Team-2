8384febacba19cb3e2cbe507ae331549529f1913
Bright Lesi <blesi@hawk.iit.edu>
2019-10-12 05:18:32 -0600

    new changes

--- fire-db/script.js
+++ fire-db/script.js
@@ -1,5 +1,19 @@
-function print(){
-    var name=document.getElementById("name").value;
-    document.location.href="index.html";
-    console.log(name);
-}
+(function(){
+
+const config = {
+    apiKey: "AIzaSyAE2oIm99Yz0lTHVidmQSRQKS9DE884j3E",
+    authDomain: "hackathon-ajo-7b2f8.firebaseapp.com",
+    databaseURL: "https://hackathon-ajo-7b2f8.firebaseio.com/",
+    storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+function authenticate(){
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("pswd").value;
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(email,pass);
+    promise.catch(e=> console.log(e.message));
+    
+};
+
+}());
