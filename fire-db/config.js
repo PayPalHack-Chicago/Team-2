6dddfa4b1b460f048d70487ed704ab5bc5ae5cd6
dnov09 <dngige9@gmail.com>
2019-10-12 04:12:32 -0500

    testing blocked website

--- fire-db/config.js
+++ fire-db/config.js
@@ -1,7 +1,7 @@
 // Set the configuration for your app
 // TODO: Replace with your project's config object
 var config = {
-    apiKey: "AIzaSyAE2oIm99Yz0lTHVidmQSRQKS9DE884j3E",
+    apiKey: "",
     authDomain: "hackathon-ajo-7b2f8.firebaseapp.com",
     databaseURL: "https://hackathon-ajo-7b2f8.firebaseio.com/",
     storageBucket: "bucket.appspot.com"
